f2122e753f6d1850a381e617f207a1cd137b6043
eomhs <jesikajunior@hanmail.net>
2023-01-17 22:08:53 +0900

    Add main.go

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+type expectedJob struct {
+	id       string
+	title    string
+	location string
+	salary   string
+	summary  string
+}
+
+var baseURL string = "https://www.saramin.co.kr/zf_user/jobs/list/job-category?cat_kewd=272&page_count=50"
+
+func main() {
+	totalPages := getPages()
+
+	for i := 0; i < totalPages; i++ {
+		getPage(i)
+	}
+}
+
+func getPage(page int) {
+	pageURL := baseURL + "&page=" + strconv.Itoa(page)
+	fmt.Println("Requesting", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".list_item")
+
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		rec_id, _ := card.Attr("id")
+		id := rec_id[4:]
+		title := card.Find(".job_tit>a").Text()
+		location := card.Find(".work_place").Text()
+		fmt.Println(id, title, location)
+	})
+
+}
+
+func getPages() int {
+	pages := 0
+	res, err := http.Get(baseURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		pages = s.Find("a").Length()
+	})
+
+	return pages
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("Request failed with Status:", res.StatusCode)
+	}
+}
