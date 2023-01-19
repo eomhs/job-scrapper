41660e1f525c44e01b84427c627c8141bd88b5c3
eomhs <jesikajunior@hanmail.net>
2023-01-19 22:25:15 +0900

    Complete main feature

--- main.go
+++ main.go
@@ -1,33 +1,61 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-type expectedJob struct {
+type extractedJob struct {
 	id       string
 	title    string
 	location string
-	salary   string
-	summary  string
+	career   string
 }
 
 var baseURL string = "https://www.saramin.co.kr/zf_user/jobs/list/job-category?cat_kewd=272&page_count=50"
 
 func main() {
+	var jobs []extractedJob
 	totalPages := getPages()
 
 	for i := 0; i < totalPages; i++ {
-		getPage(i)
+		extractedJobs := getPage(i)
+		jobs = append(jobs, extractedJobs...)
+	}
+
+	writejobs(jobs)
+	fmt.Println("Done, extracted:", len(jobs))
+}
+
+func writejobs(jobs []extractedJob) {
+	file, err := os.Create("jobs.csv")
+	checkErr(err)
+
+	w := csv.NewWriter(file)
+	defer w.Flush()
+
+	headers := []string{"ID", "Title", "Location", "Career"}
+
+	wErr := w.Write(headers)
+	checkErr(wErr)
+
+	for _, job := range jobs {
+		jobSlice := []string{"https://www.saramin.co.kr/zf_user/jobs/relay/view?isMypage=no&rec_idx=" + job.id,
+			job.title, job.location, job.career}
+		jwErr := w.Write(jobSlice)
+		checkErr(jwErr)
 	}
 }
 
-func getPage(page int) {
+func getPage(page int) []extractedJob {
+	var jobs []extractedJob
 	pageURL := baseURL + "&page=" + strconv.Itoa(page)
 	fmt.Println("Requesting", pageURL)
 	res, err := http.Get(pageURL)
@@ -42,13 +70,19 @@ func getPage(page int) {
 	searchCards := doc.Find(".list_item")
 
 	searchCards.Each(func(i int, card *goquery.Selection) {
-		rec_id, _ := card.Attr("id")
-		id := rec_id[4:]
-		title := card.Find(".job_tit>a").Text()
-		location := card.Find(".work_place").Text()
-		fmt.Println(id, title, location)
+		job := extractJob(card)
+		jobs = append(jobs, job)
 	})
+	return jobs
+}
 
+func extractJob(card *goquery.Selection) extractedJob {
+	rec_id, _ := card.Attr("id")
+	id := rec_id[4:]
+	title := cleanString(card.Find(".job_tit>a").Text())
+	location := cleanString(card.Find(".work_place").Text())
+	career := cleanString(card.Find(".career").Text())
+	return extractedJob{id: id, title: title, location: location, career: career}
 }
 
 func getPages() int {
@@ -79,4 +113,8 @@ func checkCode(res *http.Response) {
 	if res.StatusCode != 200 {
 		log.Fatalln("Request failed with Status:", res.StatusCode)
 	}
+}
+
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
 }
